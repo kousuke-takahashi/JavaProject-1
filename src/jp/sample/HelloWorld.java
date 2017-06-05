18e881cbae790cd42092d48c771f4b0605222c1c
h.takahashi <walking1682@gmai.com>
2017-06-05 18:39:06 +0900

    first commit

---
+++ src/jp/sample/HelloWorld.java
@@ -0,0 +1,51 @@
+package jp.sample;
+
+public class HelloWorld {
+
+	 public static String[] data = new String[38];
+
+	public static void main (String[] args) {
+		data[0] = "学籍番号：趣味";
+		data[1] = "";
+		data[2] = "";
+		data[3] = "";
+		data[4] = "";
+		data[5] = "";
+		data[6] = "";
+		data[7] = "";
+		data[8] = "";
+		data[9] = "";
+		data[10] = "";
+		data[11] = "";
+		data[12] = "";
+		data[13] = "";
+		data[14] = "";
+		data[15] = "";
+		data[16] = "";
+		data[17] = "";
+		data[18] = "";
+		data[19] = "";
+		data[20] = "";
+		data[21] = "";
+		data[22] = "";
+		data[23] = "";
+		data[24] = "";
+		data[25] = "";
+		data[26] = "";
+		data[27] = "";
+		data[28] = "";
+		data[29] = "";
+		data[30] = "";
+		data[31] = "";
+		data[32] = "";
+		data[33] = "";
+		data[34] = "";
+		data[35] = "";
+		data[36] = "";
+		data[37] = "おわり";
+
+		for (int i = 0;i < data.length; ++i) {
+			System.out.println(data[i]);
+		}
+	}
+}
